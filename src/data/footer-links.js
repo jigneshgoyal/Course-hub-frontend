9feafea2b6a5305da82946efbffbbc9434d547fd
Jignesh Goyal <jigneshgoyal114@gmail.com>
2024-05-18 23:17:40 +0530

    final commit

---
+++ src/data/footer-links.js
@@ -0,0 +1,40 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      { title: "Al", link: "/al" },
+      { title: "Computer Science", link: "/computer-science" },
+      { title: "DevOps", link: "/devops" },
+      { title: "Game Development", link: "/game-development" },
+      { title: "Machine Learning", link: "/machine-learning" },
+      { title: "Mobile Development", link: "/mobile-development" },
+      { title: "Web Design", link: "/web-design" },
+      { title: "Web Development", link: "/web-development" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+      { title: "C++", link: "/c++" },
+      { title: "HTML & CSS", link: "/html-css" },
+      { title: "Java", link: "/java" },
+      { title: "JavaScript", link: "/javascript" },
+      { title: "Kotlin", link: "/kotlin" },
+      { title: "PHP", link: "/php" },
+      { title: "Python", link: "/python" },
+      { title: "SQL", link: "/sql" },
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+        {title: "Career paths", link: "/career-paths"},
+        {title: "Career services", link: "/career-services"},
+        {title: "Interview prep", link: "/interview-prep"},
+        {title: "Professional certification", link: "/professional-certification"},
+        {title: "-", link: "/hi"},
+        {title: "Full Catalog", link: "/full-catalog"},
+        {title: "Beta Content", link: "/beta-content"}
+    ]
+  }
+];
